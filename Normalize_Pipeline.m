dad6291df672c473074dd1db19e03e858d7c2b03
Chenjennie <33103404+Chenjennie@users.noreply.github.com>
2018-05-14 11:38:03 -0500

    Update Normalize_Pipeline.m

--- Normalize_Pipeline.m
+++ Normalize_Pipeline.m
@@ -40,10 +40,12 @@ try
                 seplocs=strfind(files(a,:),'-');
                 fnum=files(a,seplocs(end):end);
                 fname=sprintf('%s_04%d_%s%s',hdr{1,1}.PatientID,hdr{1,1}.SeriesNumber,strrep(hdr{1,1}.SeriesDescription,' ','_'),fnum);
+                fname=strrep(fname,'(','-');fname=strrep(fname,')','');
                 movefile(files(a,:),fullfile(cfolder,fname),'f');
             end
         else
             fname=sprintf('%s_04%d_%s.nii',hdr{1,1}.PatientID,hdr{1,1}.SeriesNumber,strrep(hdr{1,1}.SeriesDescription,' ','_'));
+            fname=strrep(fname,'(','-');fname=strrep(fname,')','');
             movefile(files,fullfile(cfolder,fname),'f');
         end
         
